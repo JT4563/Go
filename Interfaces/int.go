5c84be8e0d4490bfbff9ed5fb9db9c1bf5e6734b
tan jo <tanmayjoddar17@gmail.com>
2025-06-06 22:10:07 +0530

    interface example

--- Interfaces/int.go
+++ Interfaces/int.go
@@ -1,15 +1,17 @@
-package main
 import "fmt"
 
+type robot struct {
+	battery int
+}
+
+func (r robot) talk() string {
+	if r.battery > 0 {
+		return "beep boop"
+	}
+	return "died omg"
+}
+
 func main() {
-    // Our toy box that only holds toys that can talk
-    toyBox := []TalkingToy{
-        TeddyBear{}, // Allowed because it has Talk()
-        Robot{},     // Allowed because it has Talk()
-    }
-    
-    // Make all toys talk!
-    for _, toy := range toyBox {
-        fmt.Println(toy.Talk())
-    }
+	r := robot{battery: 5}
+	fmt.Println(r.talk())
 }
